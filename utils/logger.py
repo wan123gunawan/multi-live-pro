7fd26a3b890eb58bc552cd53a976476a25a83026
wan123gunawan <133235654+wan123gunawan@users.noreply.github.com>
2026-04-20 16:09:09 +0700

    Delete utils directory

--- utils/logger.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-
-def log(message, logs):
-    logs.append(f"[{datetime.now().strftime('%H:%M:%S')}] {message}")
-
-    if len(logs) > 100:
-        logs[:] = logs[-100:]
