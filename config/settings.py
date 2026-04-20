5032155c6c509f5ccc39b62bc98b1416713ba2b0
wan123gunawan <133235654+wan123gunawan@users.noreply.github.com>
2026-04-20 16:08:42 +0700

    Delete config directory

--- config/settings.py
+++
@@ -1,6 +0,0 @@
-FB_RTMP = "rtmp://live-api-s.facebook.com:80/rtmp/"
-YT_RTMP = "rtmp://a.rtmp.youtube.com/live2/"
-TT_RTMP = "rtmp://live.tiktok.com/live/"
-
-VIDEO_BITRATE = "2500k"
-AUDIO_BITRATE = "128k"
